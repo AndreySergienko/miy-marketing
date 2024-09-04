73b2228ef409935c10fb5100752cbd250f00634f
Martin <martbelmoaw@gmail.com>
2024-09-04 10:43:43 +0300

    feat: add to telegram new content

--- src/utils/links.ts
+++ src/utils/links.ts
@@ -1,9 +1,11 @@
 import * as process from 'node:process';
 
-export const goToAuthorization = () => {
-  return process.env.FRONT_URL + 'authentication/registration';
+export const goToAuthorization = (token: string) => {
+  const baseUrl = process.env.FRONT_URL + 'authentication/registration';
+  return `${baseUrl}?botToken=${token}`;
 };
 
 export const mailSupport = () => process.env.MAIL_SUPPORT;
 
-export const goToFront = (target?: string) => target ? process.env.FRONT_URL + target : process.env.FRONT_URL
+export const goToFront = (target?: string) =>
+  target ? process.env.FRONT_URL + target : process.env.FRONT_URL;
