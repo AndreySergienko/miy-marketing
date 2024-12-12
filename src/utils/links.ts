e15e054eb79188b17f3ab25ef2ac798d6bc09686
a.sergienko <a.sergienko@inetstudio.ru>
2024-12-12 20:41:29 +0500

    правки сылок

--- src/utils/links.ts
+++ src/utils/links.ts
@@ -7,9 +7,9 @@ export const goToAuthorization = (token: string) => {
 
 export const mailSupport = () => process.env.MAIL_SUPPORT;
 
-export const goToFront = (target?: string) => 'https://ya.ru';
-// target ? process.env.FRONT_URL + target : process.env.FRONT_URL;
+export const goToFront = (target?: string) =>
+  target ? process.env.FRONT_URL + target : process.env.FRONT_URL;
 
-export const goToChannel = () => 'https://ya.ru';
+export const goToChannel = () => process.env.CHANNEL_URL;
 
-export const goToChatForClient = () => 'https://ya.ru';
+export const goToChatForClient = () => process.env.CHAT_URL_CLIENT;
